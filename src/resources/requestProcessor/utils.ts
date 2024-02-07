cd92da3144cfd9f4be0457cce612fffd3a3f396b
Adam Neumiller <aneumill@amazon.com>
2024-02-07 16:46:42 -0500

    fix: README and Scheduler

--- src/resources/requestProcessor/utils.ts
+++ src/resources/requestProcessor/utils.ts
@@ -7,7 +7,6 @@ import {
 import {
   ChimeSDKVoiceClient,
   CreateSipMediaApplicationCallCommand,
-  CreateSipMediaApplicationCallCommandOutput,
 } from '@aws-sdk/client-chime-sdk-voice';
 import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
 import {
@@ -30,7 +29,14 @@ const schedulerClient = new SchedulerClient({ region: AWS_REGION });
 const bedrockClient = new BedrockRuntimeClient({ region: AWS_REGION });
 const chimeSdkClient = new ChimeSDKVoiceClient({ region: AWS_REGION });
 
-export const createPrompt = (meetingInvitation: string): string => {
+interface httpResponse {
+  statusCode: number;
+  body: String;
+}
+
+
+export //Prompt
+const createPrompt = (meetingInvitation: string): string => {
   return JSON.stringify({
     prompt: `Human:${meetingInvitation} You are a an information extracting bot. Go over the ${meetingInvitation} and determine what the meeting id and meeting type are <instructions></instructions> xml tags
         
@@ -44,11 +50,10 @@ export const createPrompt = (meetingInvitation: string): string => {
               - Remove all spaces from the meeting ID (e.g., #### ## #### -> ##########). 
 
           2. If Google -  Instructions Extract Meeting ID and Dial in 
-            - For Google only, the ${meetingInvitation} will call a meetingID a 'pin', so treat it as a meetingID
-            - Remove all spaces from the meeting ID (e.g., #### ## #### -> ##########). 
-            - Extract Google and Microsoft Dial-In Number (if applicable):
-            - If the meeting is a Google meeting, extract the unique dial-in number.
-            - Locate the dial-in number following the text "to join by phone dial."
+            - For Google only, the ${meetingInvitation} will call a meetingID a 'PIN', so treat it as a meetingID
+            - Remove all spaces from the PIN (e.g., #### ## #### -> ##########). 
+            - Extract Google the dialIn number
+            - Locate the dial-in number following the text "otherwise, to join by phone"
             - Format the extracted Google dial-in number as (+1 ###-###-####), removing dashes and spaces. For example +1 111-111-1111 would become +11111111111)
 
           3. If Microsoft Teams - Instructions if meeting type is is Microsoft Teams. 
@@ -56,31 +61,35 @@ export const createPrompt = (meetingInvitation: string): string => {
             - The meetingId we want to store in the generated response is the 'Phone Conference ID' : ### ### ###
             - in the ${meetingInvitation}, there are two IDs a 'Meeting ID' (### ### ### ##) and a 'Phone Conference ID' (### ### ###), ignore the 'Meeting ID' use the 'Phone Conference ID'
             - The meetingId we want to store in the generated response is the 'Phone Conference ID' : ### ### ###
-            - The meetingID that we want is referenced as the 'Phone Conference ID' store that one as the meeting ID. 
             - Find the phone number, extract it and store it as the dialIn number (format (+1 ###-###-####), removing dashes and spaces. For example +1 111-111-1111 would become +11111111111)
     
           4. meetingType rules
           - The only valid responses for meetingType are 'Chime', 'Webex', 'Zoom', 'Google', 'Teams'
+
+          5. meetingId Format Rules 
+
+          Zoom: ### #### ####
+          Webex: #### ### ####
+          Chime: #### ## ####
+          Google: ### ### #### (last character is always '#')
+          Teams: ### ### ###
           
+          6. Other notes
+          - Ensure that the program does not create fake phone numbers and only includes the Microsoft or Google dial-in number if the meeting type is Google or Teams.
+          - Ensure that the meetingId matches perfectly.
+
           
-          5.    Generate Response:
+          7.    Generate FINAL JSON Response:
 
               - Create a response object with the following format:
               { 
                 meetingId: "meeting id goes here with spaces removed",
                 meetingType: "meeting type goes here (options: 'Chime', 'Webex', 'Zoom', 'Google', 'Teams')",
-                dialIn: "Insert Google Dial-In number with no dashes or spaces, or N/A if not a Google Meeting"
+                dialIn: "Insert Microsoft or Google Dial-In number with no dashes or spaces, or N/A if not a Google Meeting or Teams Meeting"
               }
 
               Meeting ID Formats:
 
-              Zoom: ### #### ####
-              Webex: #### ### ####
-              Chime: #### ## ####
-              Google: ### ### #### (last character is always '#')
-              Teams: ### ### ###
-
-              Ensure that the program does not create fake phone numbers and only includes the Microsoft or Google dial-in number if the meeting type is Google or Teams.
 
           </instructions>
         
@@ -113,10 +122,12 @@ export const scheduleEventBridge = async ({
   meetingID,
   meetingType,
   scheduledTime,
+  dialIn
 }: {
   meetingID: string;
   meetingType: string;
   scheduledTime: number;
+  dialIn: string;
 }): Promise<CreateScheduleOutput> => {
   const scheduledMoment = moment(scheduledTime);
   const scheduledTimeExpression = scheduledMoment
@@ -145,6 +156,7 @@ export const scheduleEventBridge = async ({
           meetingID: meetingID,
           meetingType: meetingType,
           scheduledTime: scheduledTime,
+          dialIn : dialIn
         }),
       },
     }),
@@ -179,25 +191,113 @@ export async function dialOut({
   meetingType,
   meetingID,
   scheduledTime,
+  dialIn
 }: {
   meetingType: string;
   meetingID: string;
   scheduledTime: number;
-}): Promise<CreateSipMediaApplicationCallCommandOutput> {
+  dialIn: string;
+
+}): Promise<httpResponse> {
   console.log(
     `dialing out meetingType: ${meetingType} MeetingId:${meetingID} at ${scheduledTime.toString()}`,
   );
-  const createSipMediaApplicationCallResponse = await chimeSdkClient.send(
-    new CreateSipMediaApplicationCallCommand({
-      FromPhoneNumber: SMA_PHONE,
-      ToPhoneNumber: '+18555524463',
-      SipMediaApplicationId: SMA_APP,
-      ArgumentsMap: {
-        meetingType: meetingType,
-        meetingID: meetingID,
-        scheduledTime: scheduledTime.toString(),
-      },
-    }),
-  );
-  return createSipMediaApplicationCallResponse;
+  if (meetingType && meetingID && scheduledTime) {
+    console.log('SMA Caller Initiated');
+    try {
+      if (meetingType === 'Chime') {
+        console.log('Chime');
+        const response = await chimeSdkClient.send(
+          new CreateSipMediaApplicationCallCommand({
+            FromPhoneNumber: SMA_PHONE,
+            ToPhoneNumber: '+18555524463',
+            SipMediaApplicationId: SMA_APP,
+            ArgumentsMap: {
+              meetingType: meetingType,
+              meetingID: meetingID,
+            },
+          }),
+        );
+        console.log(response);
+      } else if (meetingType === 'Webex') {
+        console.log('Webex');
+        const response = await chimeSdkClient.send(
+          new CreateSipMediaApplicationCallCommand({
+            FromPhoneNumber: SMA_PHONE,
+            ToPhoneNumber: '+18446213956',
+            SipMediaApplicationId: SMA_APP,
+            ArgumentsMap: {
+              meetingType: meetingType,
+              meetingID: meetingType
+            },
+          }),
+        );
+        console.log(response);
+      } else if (meetingType === 'Zoom') {
+        console.log('Zoom');
+        const response = await chimeSdkClient.send(
+          new CreateSipMediaApplicationCallCommand({
+            FromPhoneNumber: SMA_PHONE,
+            ToPhoneNumber: '+13017158592',
+            SipMediaApplicationId: SMA_APP,
+            ArgumentsMap: {
+              meetingType: meetingType,
+              meetingID: meetingID
+            },
+          }),
+        );
+        console.log(response);
+      } else if (meetingType === 'Google') {
+        console.log('Google');
+        const response = await chimeSdkClient.send(
+          new CreateSipMediaApplicationCallCommand({
+            FromPhoneNumber: SMA_PHONE,
+            ToPhoneNumber: dialIn,
+            SipMediaApplicationId: SMA_APP,
+            ArgumentsMap: {
+              meetingType: meetingType,
+              meetingID: meetingID,
+            },
+          }),
+        );
+        console.log(response);
+      } else if (meetingType === 'Teams') {
+        console.log('Teams');
+        console.log(dialIn, meetingType, meetingID)
+        const response = await chimeSdkClient.send(
+          new CreateSipMediaApplicationCallCommand({
+            FromPhoneNumber: SMA_PHONE,
+            ToPhoneNumber: dialIn,
+            SipMediaApplicationId: SMA_APP,
+            ArgumentsMap: {
+              meetingType: meetingType,
+              meetingID: meetingID
+            },
+          }),
+        );
+        console.log(response);
+      } else {
+        console.log('To be Built soon');
+      }
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ Message: 'Call initiated successfully' }),
+      };
+    } catch (error: any) {
+      console.error(error);
+
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ Error: error.message }),
+      };
+    }
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ Error: 'Missing required parameters' }),
+    };
+  }
 }
+
+
