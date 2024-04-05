6092bcc0cb2de8e7cbaab39ff7bdca65181719b7
Adam Neumiller <aneumill@amazon.com>
2024-04-05 13:57:06 -0400

    fix: upgrade prompts

--- src/resources/requestProcessor/utils.ts
+++ src/resources/requestProcessor/utils.ts
@@ -369,21 +369,26 @@ const getAllScheduleDetails = async () => {
   }
 };
 
+     
 const createPrompt = (meetingInvitation: string): string => {
   return JSON.stringify({
-    prompt: `Human:${meetingInvitation} You are a an information extracting bot. Go over the ${meetingInvitation} and determine what the meeting id and meeting type are <instructions></instructions> xml tags
+    prompt: `Human: You are a an information extracting bot. Go over the meeting invitation below and determine what the meeting id and meeting type are <instructions></instructions> xml tags
         
-          <instructions>  
+            <meeting_invitation>  
+                    ${meetingInvitation}
+            </meeting_invitation> 
+
+            <instructions>  
 
           1. Identify Meeting Type:
-              Determine if the ${meetingInvitation} is for Chime, Zoom, Google, Microsoft Teams, or Webex meetings.
+              Determine if the meeting invitation is for Chime, Zoom, Google, Microsoft Teams, or Webex meetings.
 
           2. Chime, Zoom, and Webex
               - Find the meetingID
               - Remove all spaces from the meeting ID (e.g., #### ## #### -> ##########). 
 
           2. If Google -  Instructions Extract Meeting ID and Dial in 
-            - For Google only, the ${meetingInvitation} will call a meetingID a 'PIN', so treat it as a meetingID
+            - For Google only, the meeting invitation will call a meetingID a 'PIN', so treat it as a meetingID
             - Remove all spaces from the PIN (e.g., #### ## #### -> ##########). 
             - Extract Google the dialIn number
             - Locate the dial-in number following the text "otherwise, to join by phone"
@@ -392,7 +397,7 @@ const createPrompt = (meetingInvitation: string): string => {
           3. If Microsoft Teams - Instructions if meeting type is is Microsoft Teams. 
             - Pay attention to these instructions carefully            
             - The meetingId we want to store in the generated response is the 'Phone Conference ID' : ### ### ###
-            - in the ${meetingInvitation}, there are two IDs a 'Meeting ID' (### ### ### ##) and a 'Phone Conference ID' (### ### ###), ignore the 'Meeting ID' use the 'Phone Conference ID'
+            - in the meeting invitation, there are two IDs a 'Meeting ID' (### ### ### ##) and a 'Phone Conference ID' (### ### ###), ignore the 'Meeting ID' use the 'Phone Conference ID'
             - The meetingId we want to store in the generated response is the 'Phone Conference ID' : ### ### ###
             - Find the phone number, extract it and store it as the dialIn number (format (+1 ###-###-####), removing dashes and spaces. For example +1 111-111-1111 would become +11111111111)
     
